8c9abd8a946976701b532470f4dfe5c5a8f198cf
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-06-21 13:50:28 +0200

    feat: 1.0 rewrite

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
-import { dirname, extname, resolve, relative } from 'path'
+import { resolve, relative } from 'path'
 import fs from 'fs'
 import process from 'node:process'
 import FastGlob from 'fast-glob'
 import lodash from 'lodash'
 import Handlebars from 'handlebars'
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
+import { getPackageInfo, merge, pluginBundle, pluginError, pluginReload, processData } from 'vituum/utils/common.js'
+import { renameBuildEnd, renameBuildStart } from 'vituum/utils/build.js'
 
-const { name } = JSON.parse(fs.readFileSync(resolve(dirname((fileURLToPath(import.meta.url))), 'package.json')).toString())
+const { name } = getPackageInfo(import.meta.url)
+
+/**
+ * @type {import('@vituum/vite-plugin-handlebars/types').PluginUserConfig}
+ */
 const defaultOptions = {
     reload: true,
     root: null,
     helpers: {},
-    globals: {},
     partials: {
         directory: null,
         extname: true
     },
-    data: '',
-    filetypes: {
-        html: /.(json.html|hbs.json.html|hbs.html)$/,
-        json: /.(json.hbs.html)$/
-    },
+    globals: {},
+    data: ['src/data/**/*.json'],
+    formats: ['hbs', 'json.hbs', 'json'],
     handlebars: {
         compileOptions: {},
         runtimeOptions: {}
     }
 }
 
-function processData(paths, data = {}) {
-    let context = {}
-
-    lodash.merge(context, data)
-
-    const normalizePaths = Array.isArray(paths) ? paths.map(path => path.replace(/\\/g, '/')) : paths.replace(/\\/g, '/')
-
-    FastGlob.sync(normalizePaths).forEach(entry => {
-        const path = resolve(process.cwd(), entry)
-
-        context = lodash.merge(context, JSON.parse(fs.readFileSync(path).toString()))
-    })
-
-    return context
-}
-
-const renderTemplate = async(filename, content, options) => {
+const renderTemplate = async ({ filename, server, root }, content, options) => {
+    const initialFilename = filename.replace('.html', '')
     const output = {}
-    const context = options.data ? processData(options.data, options.globals) : options.globals
+    const context = options.data
+        ? processData({
+            paths: options.data,
+            root
+        }, options.globals)
+        : options.globals
 
-    const isJson = filename.endsWith('.json.html') || filename.endsWith('.json')
-    const isHtml = filename.endsWith('.html') && !options.filetypes.html.test(filename) && !options.filetypes.json.test(filename) && !isJson
+    if (initialFilename.endsWith('.json')) {
+        lodash.merge(context, JSON.parse(content))
 
-    if (isJson || isHtml) {
-        lodash.merge(context, JSON.parse(fs.readFileSync(filename).toString()))
+        if (!options.formats.includes(context.format)) {
+            return new Promise((resolve) => {
+                output.content = content
+                resolve(output)
+            })
+        }
 
-        content = `{{> (lookup @root 'template')}}`
+        content = '{{> (lookup @root \'template\')}}'
 
         if (typeof context.template === 'undefined') {
-            console.error(chalk.red(name + ' template must be defined - ' + filename))
+            const error = `${name}: template must be defined for file ${initialFilename}`
+
+            return new Promise((resolve) => {
+                output.error = error
+                resolve(output)
+            })
         }
 
         context.template = relative(process.cwd(), context.template).startsWith(relative(process.cwd(), options.root)) ? resolve(process.cwd(), context.template) : resolve(options.root, context.template)
 
         context.template = relative(options.root, context.template)
-    } else if (fs.existsSync(filename + '.json')) {
-        lodash.merge(context, JSON.parse(fs.readFileSync(filename + '.json').toString()))
+    } else if (fs.existsSync(`${initialFilename}.json`)) {
+        lodash.merge(context, JSON.parse(fs.readFileSync(`${initialFilename}.json`).toString()))
     }
 
     const partialGlob = !options.partials.directory ? `${options.root}/**/*.hbs` : `${resolve(process.cwd(), options.partials.directory)}/**/*.hbs`
@@ -82,78 +82,88 @@ const renderTemplate = async(filename, content, options) => {
         })
     }
 
-    try {
-        const template = Handlebars.compile(content, options.handlebars.compileOptions)
+    return new Promise((resolve) => {
+        try {
+            const template = Handlebars.compile(content, options.handlebars.compileOptions)
 
-        output.content = template(context, options.handlebars.runtimeOptions)
-    } catch (error) {
-        output.error = error
-    }
+            output.content = template(context, options.handlebars.runtimeOptions)
 
-    return output
+            resolve(output)
+        } catch (error) {
+            output.error = error
+
+            resolve(output)
+        }
+    })
 }
 
+/**
+ * @param {import('@vituum/vite-plugin-handlebars/types').PluginUserConfig} options
+ * @returns [import('vite').Plugin]
+ */
 const plugin = (options = {}) => {
-    options = lodash.merge(defaultOptions, options)
+    let resolvedConfig
+    let userEnv
 
-    return {
+    options = merge(defaultOptions, options)
+
+    return [{
         name,
-        config: ({ root }) => {
+        config (userConfig, env) {
+            userEnv = env
+        },
+        configResolved (config) {
+            resolvedConfig = config
+
             if (!options.root) {
-                options.root = root
+                options.root = config.root
             }
         },
-        transformIndexHtml: {
-            enforce: 'pre',
-            async transform(content, { path, filename, server }) {
-                path = path.replace('?raw', '')
-                filename = filename.replace('?raw', '')
+        buildStart: async () => {
+            if (userEnv.command !== 'build') {
+                return
+            }
 
+            await renameBuildStart(resolvedConfig.build.rollupOptions.input, options.formats)
+        },
+        buildEnd: async () => {
+            if (userEnv.command !== 'build') {
+                return
+            }
+
+            await renameBuildEnd(resolvedConfig.build.rollupOptions.input, options.formats)
+        },
+        transformIndexHtml: {
+            order: 'pre',
+            async transform (content, { filename, server }) {
                 if (
-                    !options.filetypes.html.test(path) &&
-                    !options.filetypes.json.test(path) &&
-                    !content.startsWith('<script type="application/json" data-format="hbs"')
+                    !options.formats.find(format => filename.replace('.html', '').endsWith(format)) ||
+                    (filename.replace('.html', '').endsWith('.json') && !content.startsWith('{'))
                 ) {
                     return content
                 }
 
-                if (content.startsWith('<script type="application/json" data-format="hbs"')) {
-                    const matches = content.matchAll(/<script\b[^>]*data-format="(?<format>[^>]+)"[^>]*>(?<data>[\s\S]+?)<\/script>/gmi)
-
-                    for (const match of matches) {
-                        content = JSON.parse(match.groups.data)
-                    }
+                if (
+                    (filename.replace('.html', '').endsWith('.json') && content.startsWith('{')) &&
+                    (JSON.parse(content)?.format && !options.formats.includes(JSON.parse(content)?.format))
+                ) {
+                    return content
                 }
 
-                const render = await renderTemplate(filename, content, options)
+                const render = await renderTemplate({ filename, server, root: resolvedConfig.root }, content, options)
+                const renderError = pluginError(render.error, server, name)
 
-                if (render.error) {
-                    if (!server) {
-                        console.error(chalk.red(render.error))
-                        return
-                    }
-
-                    setTimeout(() => server.ws.send({
-                        type: 'error',
-                        err: {
-                            message: render.error.message,
-                            plugin: name
-                        }
-                    }), 50)
+                if (renderError && server) {
+                    return
+                } else if (renderError) {
+                    return renderError
                 }
 
                 return render.content
             }
         },
-        handleHotUpdate({ file, server }) {
-            if (
-                (typeof options.reload === 'function' && options.reload(file)) ||
-                (options.reload && (options.filetypes.html.test(file) || options.filetypes.json.test(file)))
-            ) {
-                server.ws.send({ type: 'full-reload' })
-            }
-        }
-    }
+        handleHotUpdate: ({ file, server }) => pluginReload({ file, server }, options)
+    }, pluginBundle(options.formats)]
 }
 
 export default plugin
---
+++ types/index.d.ts
@@ -0,0 +1,50 @@
+type KnownHelpers = {
+    [name in BuiltinHelperName | CustomHelperName]: boolean;
+};
+
+type BuiltinHelperName =
+    "helperMissing"|
+    "blockHelperMissing"|
+    "each"|
+    "if"|
+    "unless"|
+    "with"|
+    "log"|
+    "lookup";
+
+type CustomHelperName = string;
+
+interface CompileOptions {
+    data?: boolean;
+    compat?: boolean;
+    knownHelpers?: KnownHelpers;
+    knownHelpersOnly?: boolean;
+    noEscape?: boolean;
+    strict?: boolean;
+    assumeObjects?: boolean;
+    preventIndent?: boolean;
+    ignoreStandalone?: boolean;
+    explicitPartialContext?: boolean;
+}
+
+interface HandlebarsOptions {
+    compileOptions?: CompileOptions,
+    runtimeOptions?: import('handlebars').ParseOptions
+}
+
+interface PartialsOptions {
+    directory?: string,
+    extname?: boolean
+}
+
+export interface PluginUserConfig {
+    reload?: boolean | Function
+    root?: string
+    helpers?: Object
+    filters?: Object
+    partials?: PartialsOptions
+    globals?: Object
+    data?: string | string[]
+    formats?: string[]
+    handlebars?: HandlebarsOptions
+}
